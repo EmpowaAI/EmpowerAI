ac15bb17fd63443e47c75da70b9a3f6e99f81a70
Nicolette Mashaba <nicolette.mashaba@techbridlefoundation.org>
2026-01-30 21:09:02 +0200

    FULL RESTORE: Complete working app with all components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
-// App.tsx
 import { Routes, Route, Navigate } from 'react-router-dom'
-import { UserProvider, useUser } from './lib/user-context'
+import { useUser } from './lib/user-context'
 import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
@@ -25,32 +24,27 @@ function ProtectedRoute({ children }: { children: React.ReactNode }) {
 
 function App() {
   return (
-    <UserProvider>
-      <Routes>
-        {/* Public routes */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        
-        {/* Protected dashboard routes */}
-        <Route path="/dashboard" element={
-          <ProtectedRoute>
-            <DashboardLayout />
-          </ProtectedRoute>
-        }>
-          <Route index element={<Dashboard />} />
-          <Route path="cv-analyzer" element={<CVAnalyzer />} />
-          <Route path="twin" element={<TwinBuilder />} />
-          <Route path="simulations" element={<Simulations />} />
-          <Route path="opportunities" element={<Opportunities />} />
-          <Route path="interview" element={<InterviewCoach />} />
-        </Route>
-        
-        {/* Fallback route */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </UserProvider>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      
+      <Route path="/dashboard" element={
+        <ProtectedRoute>
+          <DashboardLayout />
+        </ProtectedRoute>
+      }>
+        <Route index element={<Dashboard />} />
+        <Route path="cv-analyzer" element={<CVAnalyzer />} />
+        <Route path="twin" element={<TwinBuilder />} />
+        <Route path="simulations" element={<Simulations />} />
+        <Route path="opportunities" element={<Opportunities />} />
+        <Route path="interview" element={<InterviewCoach />} />
+      </Route>
+      
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
-
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import { ThemeProvider } from "./lib/theme"
+import { UserProvider } from "./lib/user-context"
 import App from "./App"
 import "./index.css"
 
@@ -10,8 +10,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
       <BrowserRouter>
-        <App />
+        <UserProvider>
+          <App />
+        </UserProvider>
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>,
-);
+  </React.StrictMode>
+)
